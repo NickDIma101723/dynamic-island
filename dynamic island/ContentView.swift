a9cf0aa63b38972d01dd20f6c5e90ea94a354ecc
NickDIma101723 <101723@glr.nl>
2026-03-17 13:37:26 +0100

    Initial Commit

---
+++ dynamic island/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dynamic island
+//
+//  Created by Niko Dima on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dynamic island/dynamic_islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamic_islandApp.swift
+//  dynamic island
+//
+//  Created by Niko Dima on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct dynamic_islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
